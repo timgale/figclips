985cc2eca711d097efef80e69256bb2c6a05b846
Tim Gale <timgale@tims-mbp.home>
2021-12-22 14:34:59 +0000

    update holding page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 // Step 1: Import React
 import * as React from 'react'
-import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
+// import Layout from '../components/layout'
+// import { StaticImage } from 'gatsby-plugin-image'
 import { 
   container,
 } from '../components/layout.module.css'
@@ -9,13 +9,16 @@ import {
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home">
-      <p>Welcome to FigClips! Your regular dose of Figma tips.</p>
-      <StaticImage
-        alt="Text image"
-        src="https://via.placeholder.com/500"
-      />
-    </Layout>
+    <div className={container}>
+      <h1>FigClips</h1>
+    </div>
+    // <Layout pageTitle="Home">
+    //   <p>Welcome to FigClips! Your regular dose of Figma tips.</p>
+    //   <StaticImage
+    //     alt="Text image"
+    //     src="https://via.placeholder.com/500"
+    //   />
+    // </Layout>
   )
 }
 // Step 3: Export your component
